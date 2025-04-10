c94d11bdbeea4c71fa4191b7a0c31790fcf34c2d
Josue De Los Santos <joshcoder2000@gmail.com>
2025-04-10 15:28:05 -0300

    refactor: refactor routes for better security

--- app/api/deleteimage/route.ts
+++ app/api/deleteimage/route.ts
@@ -1,6 +1,13 @@
+import type { NextRequest } from "next/server";
 import { deleteFileAuto } from "@/lib/actions";
 
-export async function GET() {
+export async function GET(request: NextRequest) {
+  const authHeader = request.headers.get("authorization");
+  if (authHeader !== `Bearer ${process.env.CRON_SECRET}`) {
+    return new Response("Unauthorized", {
+      status: 401,
+    });
+  }
   await deleteFileAuto();
   return new Response("File deleted successfully");
 }
