93c762afc4911d14824b8e04470f944d066f802e
Josue De Los Santos <joshcoder2000@gmail.com>
2025-04-10 15:12:48 -0300

    refactor: secures uploadimage route

--- app/api/uploadimage/route.ts
+++ app/api/uploadimage/route.ts
@@ -1,6 +1,13 @@
+import type { NextRequest } from "next/server";
 import { uploadFileAuto } from "@/lib/actions";
 
-export async function GET() {
+export async function GET(request: NextRequest) {
+  const authHeader = request.headers.get("authorization");
+  if (authHeader !== `Bearer ${process.env.CRON_SECRET}`) {
+    return new Response("Unauthorized", {
+      status: 401,
+    });
+  }
   await uploadFileAuto();
   return new Response("File uploaded successfully");
 }
